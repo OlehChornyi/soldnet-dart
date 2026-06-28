9f69cbc98fca8dbb12dd1d4b8d8391d4034a7c7d
oleh <phd.chornyi@gmail.com>
2026-06-28 11:08:57 +0300

    comment

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:soldnet/app/app.dart';
 
 void main() => runApp(ProviderScope(child: const App()));
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// 2026.06.28 | Add sounds to the app.
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
