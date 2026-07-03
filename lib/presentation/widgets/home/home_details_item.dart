cc5f82804db150b220b055e4d2e164b69328e902
oleh <phd.chornyi@gmail.com>
2026-07-03 14:14:26 +0300

    homePage: details open url

--- lib/presentation/widgets/home/home_details_item.dart
+++ lib/presentation/widgets/home/home_details_item.dart
@@ -3,82 +3,90 @@ import 'package:flutter_svg/svg.dart';
 import 'package:soldnet/models/data/home_sections.dart';
 import 'package:soldnet/presentation/theme/app_colors.dart';
 import 'package:soldnet/presentation/theme/app_text_styles.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomeDetailsItem extends StatelessWidget {
   const HomeDetailsItem({super.key, required this.detail});
 
   final SectionDetail detail;
 
+  void _openUrl() async {
+    await launchUrl(Uri.parse(detail.url));
+  }
+
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
 
-    return Container(
-      padding: const EdgeInsets.only(bottom: 16),
-      decoration: BoxDecoration(
-          // color: AppColors.white.withAlpha(150),
-          border: Border.all(
-            width: 1,
-            color: AppColors.black,
-          ),
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(4),
-              topRight: Radius.circular(12),
-              bottomLeft: Radius.circular(12),
-              bottomRight: Radius.circular(12))),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            width: screenWidth - 32,
-            constraints: BoxConstraints(minHeight: 36),
-            padding: const EdgeInsets.only(right: 16),
-            decoration: BoxDecoration(
-                color: AppColors.black,
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(2),
-                    topRight: Radius.circular(10))),
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 6),
-                child: Row(
-                  children: [
-                    const SizedBox(width: 4),
-                    SvgPicture.asset(
-                      'assets/icons/home/target.svg',
-                      colorFilter: ColorFilter.mode(
-                        AppColors.white,
-                        BlendMode.srcIn,
+    return GestureDetector(
+      onTap: _openUrl,
+      child: Container(
+        padding: const EdgeInsets.only(bottom: 16),
+        decoration: BoxDecoration(
+            // color: AppColors.white.withAlpha(150),
+            border: Border.all(
+              width: 1,
+              color: AppColors.black,
+            ),
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(4),
+                topRight: Radius.circular(12),
+                bottomLeft: Radius.circular(12),
+                bottomRight: Radius.circular(12))),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: screenWidth - 32,
+              constraints: BoxConstraints(minHeight: 36),
+              padding: const EdgeInsets.only(right: 16),
+              decoration: BoxDecoration(
+                  color: AppColors.black,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(2),
+                      topRight: Radius.circular(10))),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 6),
+                  child: Row(
+                    children: [
+                      const SizedBox(width: 4),
+                      SvgPicture.asset(
+                        'assets/icons/home/target.svg',
+                        colorFilter: ColorFilter.mode(
+                          AppColors.white,
+                          BlendMode.srcIn,
+                        ),
+                        width: 18,
                       ),
-                      width: 18,
-                    ),
-                    const SizedBox(width: 4),
-                    SizedBox(
-                      width: screenWidth - 76,
-                      child: Text(
-                        detail.label,
-                        textAlign: TextAlign.left,
-                        style: AppTextStyles.s16w700(color: AppColors.white),
+                      const SizedBox(width: 4),
+                      SizedBox(
+                        width: screenWidth - 76,
+                        child: Text(
+                          detail.label,
+                          textAlign: TextAlign.left,
+                          style: AppTextStyles.s16w700(color: AppColors.white),
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-          const SizedBox(
-            height: 8,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Text(
-              detail.description,
-              textAlign: TextAlign.left,
-              style: AppTextStyles.s12w500(color: AppColors.grey50),
+            const SizedBox(
+              height: 8,
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Text(
+                detail.description,
+                textAlign: TextAlign.left,
+                style: AppTextStyles.s12w500(color: AppColors.grey50),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
