e85fa85cb5542f685b0771d514be80038ab0c9df
oleh <phd.chornyi@gmail.com>
2026-07-17 17:20:51 +0300

    chatPage: header animation

--- lib/presentation/widgets/chat/chat_header.dart
+++ lib/presentation/widgets/chat/chat_header.dart
@@ -1,3 +1,5 @@
+import 'package:flip_card/flip_card.dart';
+import 'package:flip_card/flip_card_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:soldnet/models/utils/chat_tab.dart';
@@ -7,23 +9,94 @@ import 'package:soldnet/presentation/widgets/app/button/app_button_fade.dart';
 import 'package:soldnet/presentation/widgets/chat/chat_bottom_sheet_users.dart';
 import 'package:soldnet/stores/store_chat.dart';
 
-class ChatHeader extends ConsumerWidget {
+class ChatHeader extends ConsumerStatefulWidget {
   const ChatHeader({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final chatState = ref.watch(storeChatProvider);
+  ConsumerState<ChatHeader> createState() => _ChatHeaderState();
+}
 
-    return Container(
-      width: MediaQuery.of(context).size.width - 92,
-      height: 106,
-      padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(12),
-        color: AppColors.active,
-      ),
-      child: chatState.tab == ChatTab.dialog
-          ? Column(
+class _ChatHeaderState extends ConsumerState<ChatHeader> {
+  final flipController = FlipCardController();
+
+  @override
+  Widget build(BuildContext context) {
+    ref.listen(
+      storeChatProvider.select((state) => state.tab),
+      (previous, next) {
+        if (next == ChatTab.groups &&
+            !(flipController.state?.isFront ?? true)) {
+          flipController.toggleCard();
+        }
+
+        if (next == ChatTab.dialog &&
+            (flipController.state?.isFront ?? false)) {
+          flipController.toggleCard();
+        }
+      },
+    );
+    final chatState = ref.watch(storeChatProvider);
+    final chatNotifier = ref.read(storeChatProvider.notifier);
+
+    return FlipCard(
+        side: chatState.tab == ChatTab.groups ? CardSide.FRONT : CardSide.BACK,
+        flipOnTouch: false,
+        controller: flipController,
+        front: Container(
+          width: MediaQuery.of(context).size.width - 92,
+          height: 106,
+          padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            color: AppColors.active,
+          ),
+          child: Center(
+              child: Column(
+            children: [
+              GestureDetector(
+                onTap: () {
+                  chatNotifier.getAllUsers();
+                  showChatBottomSheetUsers();
+                },
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.add_rounded,
+                        color: AppColors.white,
+                      ),
+                      Text(
+                        'Додати Чат',
+                        style: AppTextStyles.s16w500(color: AppColors.white),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Divider(
+                color: AppColors.white,
+              ),
+              const SizedBox(
+                height: 4,
+              ),
+              Text(
+                'У вас є 8 активних чатів',
+                style: AppTextStyles.s16w500(color: AppColors.white),
+              )
+            ],
+          )),
+        ),
+        back: Container(
+            width: MediaQuery.of(context).size.width - 92,
+            height: 106,
+            padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: AppColors.active,
+            ),
+            child: Column(
               children: [
                 Padding(
                   padding: const EdgeInsets.only(top: 10),
@@ -61,45 +134,7 @@ class ChatHeader extends ConsumerWidget {
                   ],
                 )
               ],
-            )
-          : Center(
-              child: Column(
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    ref.read(storeChatProvider.notifier).getAllUsers();
-                    showChatBottomSheetUsers();
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 8),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.add_rounded,
-                          color: AppColors.white,
-                        ),
-                        Text(
-                          'Додати Чат',
-                          style: AppTextStyles.s16w500(color: AppColors.white),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-                Divider(
-                  color: AppColors.white,
-                ),
-                const SizedBox(
-                  height: 4,
-                ),
-                Text(
-                  'У вас є 8 активних чатів',
-                  style: AppTextStyles.s16w500(color: AppColors.white),
-                )
-              ],
-            )),
-    );
+            )));
   }
 }
 
