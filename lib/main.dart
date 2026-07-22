f97a98d02274af84d7260f3e10d6c7ac0e42cedd
oleh <phd.chornyi@gmail.com>
2026-07-22 17:18:07 +0300

    comment

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ void main() => runApp(ProviderScope(child: const App()));
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // 2026.07.06 | SERVER DEVELOPMENT AND INTEGRATION (WS):
 //            | DONE | 1. Authorization (+Profile(User data)).
-//            | 2. Chat: find ws or rest api (web socket).
+//            | DONE | 2. Chat: find ws or rest api (web socket).
 //            | 3. Implement ws or rest api.
 //            | 4. Images: integrate to profile and chat.
 //            | 5. Videos: integrate to chat.
@@ -42,5 +42,5 @@ void main() => runApp(ProviderScope(child: const App()));
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // 2026.07.18 | DONE | User contacts on server and in mobile.
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-// 2026.07.19 | Find how to test chat with web socket.
+// 2026.07.19 | DONE | Find how to test chat with web socket.
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
