9059febf6eb6f90d937a678d253a0a07bbca6d74
oleh <phd.chornyi@gmail.com>
2026-07-27 14:53:16 +0300

    searchPage: header ui

--- lib/presentation/widgets/search/search_container.dart
+++ lib/presentation/widgets/search/search_container.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:soldnet/presentation/widgets/search/search_header.dart';
 
 class SearchContainer extends StatelessWidget {
   const SearchContainer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    final paddingTop = MediaQuery.of(context).padding.top;
+
+    return Stack(
+      alignment: Alignment.topCenter,
+      children: [Positioned(top: paddingTop + 16, child: const SearchHeader())],
+    );
   }
 }
---
+++ lib/presentation/widgets/search/search_header.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:soldnet/presentation/theme/app_colors.dart';
+import 'package:soldnet/presentation/widgets/app/button/app_button_circle.dart';
+import 'package:soldnet/presentation/widgets/app/textfield/app_text_field.dart';
+
+class SearchHeader extends StatefulWidget {
+  const SearchHeader({super.key});
+
+  @override
+  State<SearchHeader> createState() => _SearchHeaderState();
+}
+
+class _SearchHeaderState extends State<SearchHeader> {
+  final _controller = TextEditingController();
+  final _focusNode = FocusNode();
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Container(
+      width: screenWidth - 32,
+      child: Row(
+        children: [
+          Expanded(
+            child: AppTextField(
+                controller: _controller,
+                focusNode: _focusNode,
+                isNotError: true,
+                borderRadius: 24,
+                hint: 'Пошуковий параметр тють'),
+          ),
+          const SizedBox(width: 4),
+          AppButtonCircle(
+            onTap: () {},
+            icon: Icons.filter_list,
+            buttonColor: AppColors.activeBrown,
+            iconColor: AppColors.primary,
+          ),
+          const SizedBox(width: 8),
+          AppButtonCircle(
+            onTap: () {},
+            icon: Icons.search_rounded,
+            iconColor: AppColors.activeBrown,
+          ),
+        ],
+      ),
+    );
+  }
+}
