1e671ba59823b2d3738a96c3fdf6ca0939610339
oleh <phd.chornyi@gmail.com>
2026-07-27 10:47:57 +0300

    chatPage: bottom sheet (under development)(2)

--- lib/presentation/widgets/chat/chat_bottom_sheet_group_create.dart
+++ lib/presentation/widgets/chat/chat_bottom_sheet_group_create.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:soldnet/app/app_router.dart';
 import 'package:soldnet/presentation/widgets/app/bottom_sheet/app_bottom_sheet.dart';
+import 'package:soldnet/presentation/widgets/app/button/app_button_circle.dart';
+import 'package:soldnet/presentation/widgets/app/textfield/app_text_field.dart';
 import 'package:soldnet/presentation/widgets/chat/chat_bottom_sheet_users_item.dart';
 import 'package:soldnet/stores/store_chat.dart';
 
@@ -28,6 +30,11 @@ class ChatBottomSheetGroupCreate extends ConsumerStatefulWidget {
 
 class _ChatBottomSheetGroupCreateState
     extends ConsumerState<ChatBottomSheetGroupCreate> {
+  final _controller = TextEditingController();
+  final _focusNode = FocusNode();
+
+  Future<void> _createGroup() async {}
+
   @override
   Widget build(BuildContext context) {
     final paddingBottom = MediaQuery.of(context).padding.bottom;
@@ -40,6 +47,22 @@ class _ChatBottomSheetGroupCreateState
           mainAxisSize: MainAxisSize.min,
           spacing: 8,
           children: [
+            Row(
+              children: [
+                Expanded(
+                  child: AppTextField(
+                      controller: _controller,
+                      focusNode: _focusNode,
+                      isNotError: true,
+                      hint: 'Назва групи тють'),
+                ),
+                const SizedBox(width: 8),
+                AppButtonCircle(
+                  onTap: _createGroup,
+                  icon: Icons.group_add_rounded,
+                ),
+              ],
+            ),
             if (users.isNotEmpty) ...{
               ...users.map((user) => ChatBottomSheetUsersItem(user: user))
             } else
