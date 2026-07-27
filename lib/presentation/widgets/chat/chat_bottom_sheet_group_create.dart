b55ea59b2f65a29dffd242b45931d04bf924b481
oleh <phd.chornyi@gmail.com>
2026-07-27 10:34:38 +0300

    chatPage: bottom sheet (under development)

--- lib/presentation/widgets/chat/chat_bottom_sheet_group_create.dart
+++ lib/presentation/widgets/chat/chat_bottom_sheet_group_create.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:soldnet/app/app_router.dart';
+import 'package:soldnet/presentation/widgets/app/bottom_sheet/app_bottom_sheet.dart';
+import 'package:soldnet/presentation/widgets/chat/chat_bottom_sheet_users_item.dart';
 import 'package:soldnet/stores/store_chat.dart';
 
 void showChatBottomSheetGroupCreate() {
@@ -16,14 +18,39 @@ void showChatBottomSheetGroupCreate() {
   );
 }
 
-class ChatBottomSheetGroupCreate extends ConsumerWidget {
+class ChatBottomSheetGroupCreate extends ConsumerStatefulWidget {
   const ChatBottomSheetGroupCreate({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ChatBottomSheetGroupCreate> createState() =>
+      _ChatBottomSheetGroupCreateState();
+}
+
+class _ChatBottomSheetGroupCreateState
+    extends ConsumerState<ChatBottomSheetGroupCreate> {
+  @override
+  Widget build(BuildContext context) {
     final paddingBottom = MediaQuery.of(context).padding.bottom;
     final users = ref.watch(storeChatProvider.select((state) => state.users));
 
-    return Container();
+    return AppBottomSheet(
+        header: 'Створити групу',
+        subHeader: 'Всього в групі: ${users.length}',
+        body: Column(
+          mainAxisSize: MainAxisSize.min,
+          spacing: 8,
+          children: [
+            if (users.isNotEmpty) ...{
+              ...users.map((user) => ChatBottomSheetUsersItem(user: user))
+            } else
+              const SizedBox(
+                height: 200,
+                child: Center(
+                  child: Text('Користувачів не знайдено'),
+                ),
+              ),
+            SizedBox(height: paddingBottom + 16),
+          ],
+        ));
   }
 }
