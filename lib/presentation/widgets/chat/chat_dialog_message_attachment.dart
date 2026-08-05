5eb116e373cd73695835527b31342913ae62e1a5
oleh <phd.chornyi@gmail.com>
2026-08-05 12:23:42 +0300

    chat attachment files

--- lib/presentation/widgets/chat/chat_dialog_message_attachment.dart
+++ lib/presentation/widgets/chat/chat_dialog_message_attachment.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:soldnet/models/const/const_info.dart';
 import 'package:soldnet/models/entities/attachment.dart';
 import 'package:soldnet/presentation/theme/app_colors.dart';
@@ -7,6 +8,18 @@ class ChatDialogMessageAttachment extends StatelessWidget {
   const ChatDialogMessageAttachment({super.key, required this.atchms});
 
   final List<Attachment> atchms;
+
+  String _getSvgIconByMimeType(String mimeType) {
+    if (mimeType.contains('pdf')) {
+      return 'assets/icons/chat/pdf.svg';
+    } else if (mimeType.contains('word')) {
+      return 'assets/icons/chat/word.svg';
+    } else if (mimeType.contains('excel') || mimeType.contains('spreads')) {
+      return 'assets/icons/chat/excel.svg';
+    } else {
+      return 'assets/icons/chat/file.svg';
+    }
+  }
 
   Widget _buildOneItemUi(double size) {
     final atchm = atchms.first;
@@ -26,11 +39,25 @@ class ChatDialogMessageAttachment extends StatelessWidget {
         padding: const EdgeInsets.fromLTRB(12, 8, 12, 0),
         child: Container(
           width: size * 2 - 24,
+          padding: const EdgeInsets.all(8),
           decoration: BoxDecoration(
             color: AppColors.white,
             borderRadius: BorderRadius.circular(8),
           ),
-          child: Text(atchm.name),
+          child: Row(
+            children: [
+              SvgPicture.asset(
+                _getSvgIconByMimeType(atchm.mimeType),
+              ),
+              const SizedBox(width: 8),
+              Expanded(
+                child: Text(
+                  atchm.name,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              )
+            ],
+          ),
         ),
       );
     } else if (atchm.mimeType.startsWith('video')) {}
