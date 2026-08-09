b76ca156faab220eafcd37c476766c2c7dd8daac
oleh <phd.chornyi@gmail.com>
2026-08-09 16:42:26 +0300

    message delete (under construction)

---
+++ lib/presentation/widgets/chat/chat_popup_message_delete.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ChatPopupMessageDelete extends StatelessWidget {
+  const ChatPopupMessageDelete({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
