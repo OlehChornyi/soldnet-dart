a47b3d8d35cc68a39016c0a9ed9fed7a57bfa369
oleh <phd.chornyi@gmail.com>
2026-08-11 10:51:01 +0300

    webRTC: createConnection method

--- lib/services/call/webrtc_call.dart
+++ lib/services/call/webrtc_call.dart
@@ -22,4 +22,28 @@ class WebrtcCall {
     await localRenderer.dispose();
     await remoteRenderer.dispose();
   }
+
+  Future<void> createConnection() async {
+    final configuration = {
+      'iceServers': [
+        {
+          'urls': 'stun:stun.l.google.com:19302',
+        },
+      ],
+    };
+
+    _peerConnection = await createPeerConnection(
+      configuration,
+    );
+
+    _peerConnection!.onIceCandidate = (candidate) {
+      // Send candidate to Go through WebSocket
+    };
+
+    _peerConnection!.onTrack = (event) {
+      if (event.streams.isNotEmpty) {
+        remoteRenderer.srcObject = event.streams[0];
+      }
+    };
+  }
 }
