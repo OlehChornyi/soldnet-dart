a5a4988232a03dc1377c87e93c4f0315c9264e51
oleh <phd.chornyi@gmail.com>
2026-08-12 12:47:31 +0300

    callPage + callContainer backbone

---
+++ lib/presentation/pages/call_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:soldnet/presentation/widgets/call/call_container.dart';
+
+class CallPage extends StatelessWidget {
+  const CallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CallContainer();
+  }
+}
---
+++ lib/presentation/widgets/call/call_container.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CallContainer extends StatelessWidget {
+  const CallContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
