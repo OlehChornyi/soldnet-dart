95076d1504f0413b02379233b351c65ce4442c47
oleh <phd.chornyi@gmail.com>
2026-08-14 18:03:16 +0300

    popup delete vackbone

--- lib/presentation/widgets/chat/chat_popup_message_delete.dart
+++ lib/presentation/widgets/chat/chat_popup_message_delete.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:soldnet/presentation/theme/app_colors.dart';
 
 class ChatPopupMessageDelete extends StatelessWidget {
   const ChatPopupMessageDelete({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Dialog(
+      child: Container(
+        width: screenWidth,
+        constraints: BoxConstraints(minHeight: 100),
+        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 12),
+        decoration: BoxDecoration(
+          color: AppColors.activeBrown,
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+    );
   }
 }
