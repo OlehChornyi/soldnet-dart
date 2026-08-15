e2a3bcc400bf0d369f532a8c583813c27a69a842
oleh <phd.chornyi@gmail.com>
2026-08-15 15:00:21 +0300

    popup delete backbone(2)

--- lib/presentation/widgets/chat/chat_popup_message_delete.dart
+++ lib/presentation/widgets/chat/chat_popup_message_delete.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:soldnet/presentation/theme/app_colors.dart';
+import 'package:soldnet/presentation/widgets/app/button/app_button_action.dart';
 
 class ChatPopupMessageDelete extends StatelessWidget {
   const ChatPopupMessageDelete({super.key});
@@ -16,6 +17,32 @@ class ChatPopupMessageDelete extends StatelessWidget {
         decoration: BoxDecoration(
           color: AppColors.activeBrown,
           borderRadius: BorderRadius.circular(12),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+                width: 64,
+                height: 64,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle, color: AppColors.error),
+                child: Icon(
+                  Icons.delete_rounded,
+                  color: AppColors.white,
+                )),
+            const SizedBox(height: 24),
+            Text('TITLE'),
+            const SizedBox(height: 16),
+            Text('Subtitle'),
+            const SizedBox(height: 24),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(child: AppButtonAction(text: 'text', onTap: () {})),
+                Expanded(child: AppButtonAction(text: 'text', onTap: () {})),
+              ],
+            )
+          ],
         ),
       ),
     );
