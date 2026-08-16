eb54b36e9b397d4d7ec2b543f5dec2dc89976c7c
oleh <phd.chornyi@gmail.com>
2026-08-16 10:19:39 +0300

    popup delete message

--- lib/presentation/widgets/chat/chat_popup_message_delete.dart
+++ lib/presentation/widgets/chat/chat_popup_message_delete.dart
@@ -1,50 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:soldnet/presentation/theme/app_colors.dart';
-import 'package:soldnet/presentation/widgets/app/button/app_button_action.dart';
+import 'package:soldnet/presentation/widgets/app/popup/app_popup.dart';
 
 class ChatPopupMessageDelete extends StatelessWidget {
   const ChatPopupMessageDelete({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final screenWidth = MediaQuery.of(context).size.width;
-
-    return Dialog(
-      child: Container(
-        width: screenWidth,
-        constraints: BoxConstraints(minHeight: 100),
-        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 12),
-        decoration: BoxDecoration(
-          color: AppColors.activeBrown,
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-                width: 64,
-                height: 64,
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle, color: AppColors.error),
-                child: Icon(
-                  Icons.delete_rounded,
-                  color: AppColors.white,
-                )),
-            const SizedBox(height: 24),
-            Text('TITLE'),
-            const SizedBox(height: 16),
-            Text('Subtitle'),
-            const SizedBox(height: 24),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Expanded(child: AppButtonAction(text: 'text', onTap: () {})),
-                Expanded(child: AppButtonAction(text: 'text', onTap: () {})),
-              ],
-            )
-          ],
-        ),
-      ),
-    );
+    return AppPopup(
+        title: 'Видалити повідомлення',
+        subTitle:
+            'Повідомлення буде перманентно видалене для всіх користувачів',
+        onTap: () {});
   }
 }
