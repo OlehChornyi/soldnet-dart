75ca073ab10d4c3288afd6598e1ed9e59d9bab81
oleh <phd.chornyi@gmail.com>
2026-08-24 11:21:24 +0300

    chatPage: chat delete popup

--- lib/presentation/widgets/chat/chat_popup_conversation_delete.dart
+++ lib/presentation/widgets/chat/chat_popup_conversation_delete.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:soldnet/presentation/widgets/app/popup/app_popup.dart';
 
 class ChatPopupConversationDelete extends StatelessWidget {
   const ChatPopupConversationDelete({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return AppPopup(
+        title: 'Видалити чат',
+        subTitle: 'Чат буде перманентно видалено для всіх користувачів',
+        onTap: () {});
   }
 }
