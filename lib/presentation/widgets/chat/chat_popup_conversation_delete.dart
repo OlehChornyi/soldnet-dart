689e259c9b08d824336be62342699e26676aa5b8
oleh <phd.chornyi@gmail.com>
2026-08-25 09:40:56 +0300

    chatPage: chat delete popup (2)

--- lib/presentation/widgets/chat/chat_popup_conversation_delete.dart
+++ lib/presentation/widgets/chat/chat_popup_conversation_delete.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:soldnet/models/entities/conversation.dart';
 import 'package:soldnet/presentation/widgets/app/popup/app_popup.dart';
+import 'package:soldnet/stores/store_chat.dart';
 
-class ChatPopupConversationDelete extends StatelessWidget {
-  const ChatPopupConversationDelete({super.key});
+class ChatPopupConversationDelete extends ConsumerWidget {
+  const ChatPopupConversationDelete({super.key, required this.conversation});
+
+  final Conversation conversation;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final chatNotifier = ref.read(storeChatProvider.notifier);
+
     return AppPopup(
         title: 'Видалити чат',
         subTitle: 'Чат буде перманентно видалено для всіх користувачів',
-        onTap: () {});
+        onTap: () => chatNotifier.conversationDelete(conversation));
   }
 }
--- lib/presentation/widgets/chat/chat_popup_message_delete.dart
+++ lib/presentation/widgets/chat/chat_popup_message_delete.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:soldnet/models/entities/message.dart';
 import 'package:soldnet/presentation/widgets/app/popup/app_popup.dart';
+import 'package:soldnet/stores/store_chat.dart';
 
-class ChatPopupMessageDelete extends StatelessWidget {
-  const ChatPopupMessageDelete({super.key});
+class ChatPopupMessageDelete extends ConsumerWidget {
+  const ChatPopupMessageDelete({super.key, required this.message});
+
+  final Message message;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final chatNotifier = ref.read(storeChatProvider.notifier);
+
     return AppPopup(
         title: 'Видалити повідомлення',
         subTitle:
             'Повідомлення буде перманентно видалене для всіх користувачів',
-        onTap: () {});
+        onTap: () => chatNotifier.messageDelete(message));
   }
 }
